a76b4518dfb7316deb2321ea3688efff50037ee6
Aaron.ZhiRen.Shey <aaron.z.shey@proton.me>
2024-08-08 22:41:58 -0700

    a

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  stories: [
     "../components/**/*.mdx",
-    "../components/**/**/*.stories.@(js|jsx|mjs|ts|tsx)",],
+    "../components/**/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
